e88c3f4f120561ae478f4e078f3e7e8d8e58ef66
Iulisloi Zacarias <iuli.zacarias@gmail.com>
2016-08-25 18:01:55 -0300

    All Workinggit stgit stgit st

--- wifi/simple-mob-scanario.py
+++ wifi/simple-mob-scanario.py
@@ -10,6 +10,13 @@ from mininet.log import setLogLevel
 
 """
 Handorver example (based on proposed scenario)
+
+    Uses addresses: 
+    UAVs Mesh:  10.0.0.XXX/24
+    Hosts: 10.0.1.XXX/24
+        - 10.0.1.[1-9] - UAVs
+        - 10.0.1.[91-99] - Hosts
+
 """
 
 # Configurations
@@ -18,7 +25,7 @@ OF_CONTROLLER_PORT = 6633
 
 # Number of UAVs
 CONF_UAV_NUMBER = 9
-CONF_GUARANI_NUMBER = 9
+CONF_GUARANI_NUMBER = 4
 
 # Nodes placement
 POS_SCALE = 35
@@ -43,16 +50,16 @@ class OVSKernelSwitch13(OVSKernelSwitch):
 
 
 def uav_get_ybase():
-    y_position = POS_GUARANI_Y_START + \
+    posy = POS_GUARANI_Y_START + \
         (CONF_GUARANI_NUMBER - 1) * POS_GUARANI_DISTANCE + \
         POS_UAV_FROM_GUARANI
-    return y_position
+    return int(round(posy))
 
 
 def uav_get_xbase(uav_index):
     formula = (uav_index - (CONF_UAV_NUMBER + 1) / 2) * POS_UAV_DISTANCE
     posx = POS_GUARANI_X + formula
-    return posx
+    return int(round(posx))
 
 
 def topology():
@@ -80,17 +87,18 @@ def topology():
                                                               CONF_UAV_NUMBER)
         sta_mac = '00:00:00:00:00:{0:02d}'.format(i + 1)
         sta_name = 'sta{0:d}'.format(i + 1)
-        sta_ip = '10.0.0.{0:d}/8'.format(i + 1)
-        sta_power = 75
+        sta_ip = '10.0.0.{0:d}/24'.format(i + 1)
         # node position
-        uav_min_x = int(round(uav_get_xbase(i + 1) - POS_UAV_MOTION))
-        uav_max_x = int(round(uav_get_xbase(i + 1) + POS_UAV_MOTION))
-        uav_min_y = int(round(uav_get_ybase() - POS_UAV_MOTION))
-        uav_max_y = int(round(uav_get_ybase() + POS_UAV_MOTION))
+        uav_position = '{0:d},{1:d},0'.format(
+            uav_get_xbase(i + 1), uav_get_ybase() - 10)
+        uav_min_x = uav_get_xbase(i + 1) - POS_UAV_MOTION
+        uav_max_x = uav_get_xbase(i + 1) + POS_UAV_MOTION
+        uav_min_y = uav_get_ybase() - POS_UAV_MOTION
+        uav_max_y = uav_get_ybase() + POS_UAV_MOTION
         uav_list.append(net.addStation(sta_name, mac=sta_mac, ip=sta_ip,
                                        min_x=uav_min_x, max_x=uav_max_x,
                                        min_y=uav_min_y, max_y=uav_max_y,
-                                       txpower=sta_power, wlans=2))
+                                       wlans=2, position=uav_position))
 
     # print '*** Creating UAVs (Stations)'
     # for uav in uav_list:
@@ -100,23 +108,27 @@ def topology():
     print "*** Creating static Guaranis (Switch + AP)"
     for i in xrange(1, CONF_GUARANI_NUMBER + 1):
         print '    - Creating Guarani (Switch + AP) {0:d} of {1:d}' \
-            .format(i, 9)
+            .format(i, CONF_GUARANI_NUMBER)
         sw_name = 's{0:d}'.format(i)
-        ap_name = 'ap{0:d}'.format(i)
         sw_dpid = '000000000000000{0:d}'.format(i)
-        ap_dpid = '000000000000100{0:d}'.format(i)
-        ap_ssid = 'ssid_ap{0:d}'.format(i)
-        ap_channel = [1, 6, 11][i % 3]
-        ap_position = '{0:d},{1:d},0'.format(
-            POS_GUARANI_X,
-            POS_GUARANI_Y_START + ((i - 1) * POS_GUARANI_DISTANCE))
         sw_list.append(net.addSwitch(sw_name, dpid=sw_dpid))
-        ap_list.append(net.addBaseStation(ap_name, dpid=ap_dpid,
-                                          ssid=ap_ssid, mode='g',
-                                          channel=ap_channel,
-                                          position=ap_position))
-        # Link last created AP with last created Switch
-        net.addLink(sw_list[-1], ap_list[-1])
+
+        if i == CONF_GUARANI_NUMBER:
+            ap_name = 'ap{0:d}'.format(i)
+            ap_dpid = '000000000000100{0:d}'.format(i)
+            ap_ssid = 'ssid_ap{0:d}'.format(i)
+            print '      - {0:s} -> SSID: {1:s}'.format(ap_name, ap_ssid)
+            ap_position = '{0:d},{1:d},0'.format(
+                POS_GUARANI_X,
+                POS_GUARANI_Y_START + ((i - 1) * POS_GUARANI_DISTANCE))
+            ap_channel = [1, 6, 11][i % 3]
+
+            ap_list.append(net.addBaseStation(ap_name, dpid=ap_dpid,
+                                              ssid=ap_ssid, mode='g',
+                                              channel=ap_channel,
+                                              position=ap_position))
+            # Link last created AP with last created Switch
+            net.addLink(sw_list[-1], ap_list[-1])
 
     print "*** Creating links between Guaranis"
     for i in xrange(0, CONF_GUARANI_NUMBER - 1):
@@ -124,17 +136,18 @@ def topology():
                                                        sw_list[i + 1].name)
         net.addLink(sw_list[i], sw_list[i + 1])
 
-    h1 = net.addHost('h1', mac='00:00:00:09:00:01', ip='10.0.0.91/8')
-    h2 = net.addHost('h2', mac='00:00:00:09:00:02', ip='10.0.0.92/8')
+    h1 = net.addHost('h1', mac='00:00:00:00:01:91', ip='10.0.1.91/24')
+    h2 = net.addHost('h2', mac='00:00:00:00:01:91', ip='10.0.1.92/24')
+    """ Creating routes on hosts """
     net.addLink(h1, sw_list[0])
-    net.addLink(h2, sw_list[8])
+    net.addLink(h2, sw_list[-1])
 
     print '*** Adding Mesh network among Stations'
     for uav in uav_list:
         print '    - Adding UAV {0:s} to mesh network'.format(uav.name)
         net.addMesh(uav, ssid='meshNet')
 
-    print "*** Starting network"
+    print '*** Starting network'
     net.build()
     c1.start()
     # Starting Switches
@@ -146,17 +159,48 @@ def topology():
         print '    - Starting {0:s}'.format(ap.name)
         ap.start([c1])
 
+    print '**** Associating UAV5 to Access Point'
+    uav_list[4].cmd('iwconfig sta5-wlan1 essid ssid_ap4')
+    print '**** Configuring the IP address for UAV Wireless interface'
+    uav_list[4].cmd('ifconfig sta5-wlan1 10.0.1.5/24')
+
+    # Adding routes to hosts
+    h1.cmd('route add -net 10.0.0.0 netmask 255.255.255.0 gw 10.0.1.5')
+    h2.cmd('route add -net 10.0.0.0 netmask 255.255.255.0 gw 10.0.1.5')
+    # Enabling the routing by node sta5
+    uav_list[4].cmd(
+        'route add -net 10.0.1.0 netmask 255.255.255.0 dev sta5-wlan1')
+    uav_list[4].cmd('echo 1 > /proc/sys/net/ipv4/ip_forward')
+    # Configuring staX routes
+    uav_list[0].cmd(
+        'route add -net 10.0.1.0 netmask 255.255.255.0 gw 10.0.0.5')
+    uav_list[1].cmd(
+        'route add -net 10.0.1.0 netmask 255.255.255.0 gw 10.0.0.5')
+    uav_list[2].cmd(
+        'route add -net 10.0.1.0 netmask 255.255.255.0 gw 10.0.0.5')
+    uav_list[3].cmd(
+        'route add -net 10.0.1.0 netmask 255.255.255.0 gw 10.0.0.5')
+    # uav_list[4] is the GATEWAY!!!
+    uav_list[5].cmd(
+        'route add -net 10.0.1.0 netmask 255.255.255.0 gw 10.0.0.5')
+    uav_list[6].cmd(
+        'route add -net 10.0.1.0 netmask 255.255.255.0 gw 10.0.0.5')
+    uav_list[7].cmd(
+        'route add -net 10.0.1.0 netmask 255.255.255.0 gw 10.0.0.5')
+    uav_list[8].cmd(
+        'route add -net 10.0.1.0 netmask 255.255.255.0 gw 10.0.0.5')
+
     """uncomment to plot graph"""
     net.plotGraph(max_x=200, max_y=200)
 
     # Starting Mobility
     # net.seed(20)
-    net.startMobility(startTime=0, model='GaussMarkov', min_v=0.5, max_v=0.8)
+    # net.startMobility(startTime=0, model='GaussMarkov', min_v=0.5, max_v=0.8)
 
-    print "*** Running CLI"
+    print '*** Running CLI'
     CLI(net)
 
-    print "*** Stopping network"
+    print '*** Stopping network'
     net.stop()
 
 if __name__ == '__main__':
